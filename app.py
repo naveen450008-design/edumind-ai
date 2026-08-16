9d6648375c6ceb9718fa6fbed0e1b830509f343b
Naveen <naveen450008@gmail.com>
2026-08-16 16:40:15 +0530

    Update gunicorn

--- app.py
+++ app.py
@@ -819,4 +819,5 @@ def admin_analytics():
                            all_student_evals=all_student_evals)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
